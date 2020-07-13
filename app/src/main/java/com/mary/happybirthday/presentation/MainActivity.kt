7eca7f8c2fb7778ac0940e26f2193e4438484e0d
MaryAgeeva <maryageeva1@gmail.com>
2020-07-13 19:19:31 +0300

    (WIP) added layout for DetailFragment

--- app/src/main/java/com/mary/happybirthday/presentation/MainActivity.kt
+++ app/src/main/java/com/mary/happybirthday/presentation/MainActivity.kt
@@ -2,12 +2,41 @@ package com.mary.happybirthday.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mary.happybirthday.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_toolbar)
+        adjustNavigation()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.main_frame).navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun adjustNavigation() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navController = findNavController(R.id.main_frame)
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+
+        setUIVisibility()
+    }
+
+    private fun setUIVisibility() {
+        findNavController(R.id.main_frame).addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id) {
+                R.id.detailFragment -> supportActionBar?.show()
+                R.id.birthdayFragment -> supportActionBar?.hide()
+            }
+        }
     }
 }
